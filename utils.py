a5e4360f60830ac81b07281a3a2599ef51c5562b
patriziodegirolamo <66032528+patriziodegirolamo@users.noreply.github.com>
2022-01-15 18:45:36 +0100

    inserted color segmentation function

--- utils.py
+++ utils.py
@@ -134,19 +134,15 @@ def colour_code_segmentation(image, label_values):
         Colour coded image for segmentation visualization
     """
 
-	# w = image.shape[0]
-	# h = image.shape[1]
-	# x = np.zeros([w,h,3])
-	# colour_codes = label_values
-	# for i in range(0, w):
-	#     for j in range(0, h):
-	#         x[i, j, :] = colour_codes[int(image[i, j])]
-	label_values = [label_values[key][:3] for key in label_values if label_values[key][3] == 1]
-	label_values.append([0, 0, 0])
-	colour_codes = np.array(label_values)
-	x = colour_codes[image.astype(int)]
+    w = image.shape[0]
+    h = image.shape[1]
+    x = np.zeros([w,h,3])
+    colour_codes = label_values
+    for i in range(0, w):
+      for j in range(0, h):
+	      x[i, j, :] = colour_codes[image[i, j]]
 
-	return x
+    return x
 
 def compute_global_accuracy(pred, label):
 	pred = pred.flatten()
