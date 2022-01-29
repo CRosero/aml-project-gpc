26e1b8f2dbd7bf07a7c823390a6eef007cf92d19
giulia.dascenzi <giulia.dash@gmail.com>
2022-01-29 18:33:46 +0100

    fixed bugs

--- Scripts/eval.py
+++ Scripts/eval.py
@@ -5,12 +5,12 @@ from torch.utils.data import DataLoader
 from model.build_BiSeNet import BiSeNet
 import numpy as np
 from utils.utils import reverse_one_hot, compute_global_accuracy, fast_hist, per_class_iu, cal_miou, colour_code_segmentation
-import tqdm
+from tqdm import tqdm
 from PIL import Image
 
 
 ## -- VALIDATION --
-def val(args, model, dataloader, print=False):
+def val(args, model, dataloader, save=False):
 
     #TODO: prendere dal json
     palette = [[128,64,128],[244,35,232], [70,70,70],[102,102,156],[190,153,153],[153,153,153],[250,170,30],[220,220,0],[107,142,35],[152,251,152],[70,130,180],[220,20,60],[255,0,0],[0,0,142],[0,0,70],[0,60,100],[0,80,100],[0,0,230],[119,11,32],[0,0,0]]
@@ -48,7 +48,7 @@ def val(args, model, dataloader, print=False):
 
             precision_record.append(precision)
 
-            if print and i < 20:
+            if save and i < 20:
               # save some images
               predict = colour_code_segmentation(np.array(predict), dictionary)
               label = colour_code_segmentation(np.array(label), dictionary)
